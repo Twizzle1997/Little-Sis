5fca3d79699200141a02f1fa877d330e65cfff97
Audrey <audrey.cornaire@gmail.com>
2021-07-02 11:11:50 +0200

    Data augmentation and tensorboard

--- main/src/classes/functions.py
+++ main/src/classes/functions.py
@@ -1,8 +1,17 @@
-import pickle
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 
 class functions: 
     
-    def unpickle(file):
-        with open(file, 'rb') as fo:
-            dict = pickle.load(fo, encoding='bytes')
-        return dict
+    def create_model(self):
+        model = Sequential()
+
+        model.add(Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu', input_shape=(32, 32, 3)))
+        model.add(Conv2D(128, kernel_size=(3,3), strides=(2,2), padding='same', activation='relu', input_shape=(32, 32, 3)))
+        model.add(MaxPool2D(pool_size=(2,2)))
+        model.add(Flatten())
+        model.add(Dense(10, activation='softmax'))
+
+        model.summary()
+
+        return model
